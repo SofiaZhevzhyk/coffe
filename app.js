8fc8b7565b7e3cb36dfd9b0fbf522687cb5ae8a2
SofiaZhevzhyk <124395487+SofiaZhevzhyk@users.noreply.github.com>
2023-09-20 17:04:11 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.header-block')
+
+burger.addEventListener('click', function() {
+    menu.classList.toggle('open')
+    burger.classList.toggle('test')
+})
+
+
+
+// 
